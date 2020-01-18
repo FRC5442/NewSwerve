7a1d844bf941ffb2e1d6bde331b92353fdf2423b
jasloan02 <jasloan02@gmail.com>
2020-01-18 13:03:18 -0600

    Get rid of unused import in SwerveModule. -Sloan

--- SwerveStuff/src/main/java/frc/robot/subsystems/SwerveModule.java
+++ SwerveStuff/src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -11,7 +11,6 @@ import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.drive.Vector2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.SharedMethods;
