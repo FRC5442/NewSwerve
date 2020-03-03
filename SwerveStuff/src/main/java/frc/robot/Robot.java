abf1f206085bd40e4eb29a214594c3633176794b
Collin M <57117254+Lavertas55@users.noreply.github.com>
2020-03-02 19:32:18 -0600

    Color Sensor in Robot.java
    Color sensor capabilities added to Robot.java, use "Detected Color" value in NetworkTables for SmartDashboard - Avery & Collin

--- SwerveStuff/src/main/java/frc/robot/Robot.java
+++ SwerveStuff/src/main/java/frc/robot/Robot.java
@@ -7,10 +7,17 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commandgroups.AutoTesting;
+
+import com.revrobotics.ColorSensorV3;
+import com.revrobotics.ColorMatch;
+import com.revrobotics.ColorMatchResult;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,6 +26,20 @@ import frc.robot.commandgroups.AutoTesting;
  * project.
  */
 public class Robot extends TimedRobot {
+  
+  private final I2C.Port i2cPort = I2C.Port.kOnboard;
+  
+  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+
+  private final ColorMatch m_colorMatcher = new ColorMatch();
+
+  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.461, 0.232, 0.114);
+  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+
+  String colorString;
+
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
@@ -33,6 +54,10 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     System.out.println("Robot Initializing...");
     m_robotContainer = new RobotContainer();
+    m_colorMatcher.addColorMatch(kBlueTarget);
+    m_colorMatcher.addColorMatch(kGreenTarget);
+    m_colorMatcher.addColorMatch(kRedTarget);
+    m_colorMatcher.addColorMatch(kYellowTarget);
   }
 
   /**
@@ -49,6 +74,28 @@ public class Robot extends TimedRobot {
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
     // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
+
+    Color detectedColor = m_colorSensor.getColor();
+
+    ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+
+    if (match.color == kBlueTarget) {
+      colorString = "Blue";
+    } else if (match.color == kRedTarget) {
+      colorString = "Red";
+    } else if (match.color == kGreenTarget) {
+      colorString = "Green";
+    } else if (match.color == kYellowTarget) {
+      colorString = "Yellow";
+    } else {
+      colorString = "Unkown";
+    }
+
+    SmartDashboard.putNumber("Red", detectedColor.red);
+    SmartDashboard.putNumber("Green", detectedColor.green);
+    SmartDashboard.putNumber("Blue", detectedColor.blue);
+    SmartDashboard.putNumber("Confidence", match.confidence);
+    SmartDashboard.putString("Detected Color", colorString);
   }
 
   /**
